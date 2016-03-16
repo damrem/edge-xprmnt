ac75cf15bbab5e4e0a12aeac62e2be2080056e66
Damien Remars <dre@dolist.net>
2016-03-16 13:52:13 +0100

    cleaning

--- src/playertile/MovePlayerWithTile.hx
+++ src/playertile/MovePlayerWithTile.hx
@@ -17,65 +17,39 @@ using hxlpers.edge.EntityStaticExtension;
 class MovePlayerWithTile implements ISystem
 {
 	var timeDelta:Float;
-	var playerVel:B2Vec2;
 	var entity:Entity;
+	
 	var tileForce:B2Vec2;
-	var c:Int = 0;
+	var tileEntity:Entity;
+	var tileMovement:TileMovement;
+	var tileBody:Body;
+	
 	public function update(player:PlayerCoreComponent, playerBody:Body, tileEntityRef:TileEntityRef)
 	{
 		//trace(playerBody.b2Body.getLinearVelocity().length()*1000);
-		var tileEntity = tileEntityRef.tile;
-		var tileMovement:TileMovement = tileEntity.getFirstComponentOfType(TileMovement);
-		
-		
+		tileEntity = tileEntityRef.tile;
+		tileMovement = tileEntity.getFirstComponentOfType(TileMovement);
 		
 		if (tileMovement == null)
 		{
 			return;
 		}
-		
-		//DEBUG
-		//trace('plop');
+
 		#if debug
+		//trace('plop');
 		if(tileEntity.existsType(Opacity))	tileEntity.removeType(Opacity);
 		tileEntity.add(new Opacity(Math.random()));
 		#end
 		
+		tileBody = tileEntity.getFirstComponentOfType(Body);
 		
-		
-		
-		
-		var tileBody:Body = tileEntity.getFirstComponentOfType(Body);
-		var mass = playerBody.b2Body.getMass();
 		tileForce = tileBody.b2Body.getLinearVelocity().copy();
-		tileForce.multiply(1 / 60);
+		tileForce.multiply(timeDelta/1000);
 		
 		tileForce.add(playerBody.b2Body.getPosition());
 		
 		playerBody.b2Body.setPosition(tileForce);
 		
-		
-		
-	}
-	/*
-	public function updateAdded(entity:Entity, node: { player:PlayerCoreComponent, playerBody:Body, tileEntityRef:TileEntityRef } )
-	{
-		trace("updateAdded");
-		
-		var tileB2Body:B2Body = node.tileEntityRef.tile.getFirstComponentOfType(Body).b2Body;
-		var playerB2Body = node.playerBody.b2Body;
-		var tileVelocity = tileB2Body.getLinearVelocity();
-		trace(tileVelocity.x);
-		playerB2Body.setAwake(true);
-		playerB2Body.getLinearVelocity().add(tileVelocity);
-		trace(playerB2Body.getLinearVelocity().x);
-		//node.playerBody.b2Body.setLinearDamping(0);
 	}
 	
-	public function updateRemoved(entity:Entity, node: { player:PlayerCoreComponent, playerBody:Body, tileEntityRef:TileEntityRef } )
-	{
-		trace("udpateRemoved");
-		//node.playerBody.b2Body.setLinearDamping(0.5);
-	}
-	*/
 }
